8346b0350776808233fe7bb4e6b0475ae6dd0995
santosh <santoshnellagi740@gmail.com>
2023-05-16 18:27:22 +0530

    files are added for the project

---
+++ aceess.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWP6JXD5HH5TVRIDJ"
+  secret_key = "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"
+}
---
+++ main.tf
@@ -0,0 +1,61 @@
+locals {
+  vpc_id           = "vpc-09178ea6458964959"
+  subnet_id        = "subnet-067895001851206cc"
+  ssh_user         = "ubuntu"
+  key_name         = "project-3"
+  private_key_path = "~/Downloads/project-3.pem"
+}
+
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0dba2cb6798deb6d8"
+  subnet_id                   = "subnet-067895001851206cc"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} web.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
